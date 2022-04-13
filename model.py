6dbbdb6ca74a21287bdfed487e307ef6afb0a9cb
Victor Amaral <victoramaralce@gmail.com>
2022-04-13 19:19:20 -0300

    Criando a base de dados do projeto(classes e interacoes) + server para teste
    Danillo Souza <danillosouza1704@gmail.com>

---
+++ model.py
@@ -0,0 +1,27 @@
+from mesa import Model
+from agent import FoodAgent
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+
+class FoodModel(Model):
+    """Uma model com um numero definido de agentes"""
+
+    def __init__(self, N, width, height):
+        self.num_agents = N
+        self.grid = MultiGrid(width, height, True)
+        self.schedule = RandomActivation(self)
+
+        # cria agentes
+        for i in range(self.num_agents):
+            a = FoodAgent(i, self)
+            self.schedule.add(a)
+            self.running = True
+
+            #Adiciona agentes no mapa
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+
+    def step(self):
+        """Avance a model em um passo"""
+        self.schedule.step()
---
+++ server.py
@@ -0,0 +1,20 @@
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+from model import FoodModel
+
+def agent_portrayal(agent):
+    portrayal = {"Shape": "circle", "Filled": "True", "r": 0.5}
+
+    if agent.food > 0:
+        portrayal["Color"] = "green"
+        portrayal["Layer"] = 0
+    else:
+        portrayal["Color"] = "red"
+        portrayal["Layer"] = 1
+        portrayal["r"] = 0.2
+    return portrayal
+
+grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+server = ModularServer(FoodModel, [grid], "Natural Selection", {"N": 100, "width": 10, "height": 10})
+server.port = 8521
+server.launch()
