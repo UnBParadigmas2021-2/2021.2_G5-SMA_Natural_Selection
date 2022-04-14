6af1187f361ea99437e0adfd631849753262a602
AlineLermen <aline.helena.lermen@gmail.com>
2022-04-14 17:39:33 -0300

    Muda detalhes do grid
    Co-authored-by: CarlosFiuza <carlosfiuza1234@gmail.com>
    Co-authored-by: Isadora Soares <isadoragalvaoss@gmail.com>

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from src.Food.FoodAgent import FoodAgent
 
-from src.Food.FoodModel import FoodModel
+from src.Food.FoodSpecieModel import FoodSpecieModel
 
 
 def agent_portrayal(agent):
-    portrayal = {"Shape": "circle", "Filled": "True", "r": 0.5}
+    portrayal = {"Shape": "circle", "Filled": "True", "r": 0.5, "Layer": 0}
 
     if type(agent) is FoodAgent:
         if agent.food > 0:
@@ -14,14 +14,14 @@ def agent_portrayal(agent):
             portrayal["Layer"] = 0
             portrayal["r"] = 0.2
     else:
-        portrayal["Color"] = "red"
+        portrayal["Color"] = "blue"
         portrayal["Layer"] = 1
         portrayal["r"] = 0.2
     return portrayal
 
 
 grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
-server = ModularServer(FoodModel, [grid], "Natural Selection", {
-                       "N": 40, "width": 10, "height": 10})
+server = ModularServer(FoodSpecieModel, [grid], "Natural Selection", {
+                       "N": 5, "width": 10, "height": 10})
 server.port = 8081
 server.launch()
